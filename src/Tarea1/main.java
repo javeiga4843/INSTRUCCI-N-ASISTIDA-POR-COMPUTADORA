65730c05ac6441e562807c3a82f2757a22a7441b
javeiga4843 <149978803+javeiga4843@users.noreply.github.com>
2023-11-07 19:58:28 -0500

    Primer Commit
    Al inicio, se pedirá al usuario que elija el nivel de dificultad (1 significa que el programa debe usar sólo números de un dígito en los problemas; un nivel 2 significa que el programa debe utilizar números de dos dígitos máximo, y así en lo sucesivo) y el tipo de problema aritmético a estudiar (1 significa problemas de suma solamente, 2 problemas de resta, 3 problemas de multiplicación, 4 problemas de división y 5 significa una mezcla aleatoria de problemas de todos estos tipos).

---
+++ src/Tarea1/main.java
@@ -0,0 +1,25 @@
+
+package Tarea1;
+
+
+	import java.security.SecureRandom;
+	import java.util.Scanner;
+	public class main {
+
+	    private static final int NUMERO_PREGUNTAS = 10;
+
+	    private static final String[] RESPUESTAS_POSITIVAS = {
+	            "¡Muy bien!",
+	            "¡Excelente!",
+	            "¡Buen trabajo!",
+	            "¡Sigue así!"
+	    };
+
+	    private static final String[] RESPUESTAS_NEGATIVAS = {
+	            "No. Por favor intenta de nuevo.",
+	            "Incorrecto. Intenta una vez más.",
+	            "¡No te rindas!",
+	            "No. Sigue intentando."
+	    };
+
+	   
